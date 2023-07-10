a2a867f892ec1abf26086eab03806cb90ecf4036
Harsh Patel <hrspatel1111@gmail.com>
2023-07-10 13:09:51 +0530

    Added Minimum Charges

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const per_kg_price_of_zone_North_East = 30;
-const per_kg_price_of_zone_west = 27;
-const per_kg_price_of_zone_South = 16;
+// Constants
+const ODA_ORDER_CHARGE = 600;
 
-const state_of_zone_North_East = ["Odisha", "Orissa", "Bihar", "West Bengal", "Jharkhand", "Assam", "Meghalaya", "Tripura", "Arunachal Pradesh", "Mizoram", "Manipur", "Nagaland", "Sikkim", "Uttar Pradesh", "J&K", "Himachal Pradesh", "Chandigarh", "Uttarakhand", "Punjab", "Haryana", "Delhi"]
-const state_of_zone_west = ["Gujarat", "Maharashtra", "Goa", "Dadra & Nagar", "Chhattisgarh", "Madhya Pradesh", "Rajasthan"]
-const state_of_zone_South = ["Karnataka", "Andhra Pradesh", "Tamil Nadu", "Telangana", "Kerala", "Puducherry"]
+const per_kg_price_of_zone_North_East = 35;
+const per_kg_price_of_zone_west = 15;
+const per_kg_price_of_zone_South = 28;
+const per_kg_price_of_zone_Rest_of_India = 25;
+
+const Minimum_price_of_zone_North_East = 550;
+const Minimum_price_of_zone_west = 350;
+const Minimum_price_of_zone_South = 500;
+const Minimum_price_of_zone_Rest_of_India = 450;
+
+const state_of_zone_North_East = ["Assam", "Mizoram", "Manipur", "Nagaland", "Jammu & Kashmir", "Arunachal Pradesh", "Meghalaya", "Tripura"];
+const state_of_zone_west = ["Gujarat", "Maharashtra", "Goa", "Madhya Pradesh", "Chhattisgarh", "Dadra and Nagar Haveli", "Rajasthan"];
+const state_of_zone_South = ["Andhra Pradesh", "Karnataka", "Telangana", "Pondicherry", "Tamil Nadu", "Kerala"];
+const state_of_zone_Rest_of_India = ["Delhi", "Uttar Pradesh", "Punjab", "Orissa", "Himachal Pradesh", "Uttarakhand", "Haryana", "Bihar", "Jharkhand", "West Bengal"];
 
 const csvFilePath = 'data.csv';
 
-const reader = new FileReader();
-reader.onload = function (e) {
-    const csvData = e.target.result;
-
-    // Convert CSV to JSON
-    const rows = csvData.split('\n');
+// Function to convert CSV data to JSON
+function csv_to_json(csv_data) {
+    const rows = csv_data.split('\n');
     const header = rows[0].split(',');
     const jsonData = [];
 
@@ -28,22 +35,35 @@ reader.onload = function (e) {
     }
 
     // Set "Pin" as index
-    const pincode_details_json = {};
+    const pincode_json = {};
 
     for (let i = 0; i < jsonData.length; i++) {
         const pin = jsonData[i]['Pin'];
         delete jsonData[i]['Pin'];
-        pincode_details_json[pin] = jsonData[i];
+        pincode_json[pin] = jsonData[i];
     }
 
+    return pincode_json;
+}
+
+// FileReader instance
+const reader = new FileReader();
+
+// Event handler for file load
+reader.onload = function (e) {
+    const csvData = e.target.result;
+
+    // Convert CSV to JSON
+    const pincode_details_json = csv_to_json(csvData);
     console.log(pincode_details_json)
 
+    // Event listener for pincode input
     const pincodeInput = document.getElementById("pincode");
 
     pincodeInput.addEventListener("input", function () {
         const pincode = this.value;
         const pincode_details = pincode_details_json[pincode];
-        
+
         if (pincode_details === undefined) {
             pincodeInput.classList.add("is-invalid");
         } else {
@@ -51,7 +71,7 @@ reader.onload = function (e) {
         }
     });
 
-
+    // Event listener for form submission
     document.getElementById("calculateForm").addEventListener("submit", function (e) {
         e.preventDefault(); // Prevent form submission and page reload
         const pincodeInput = document.getElementById("pincode");
@@ -61,18 +81,20 @@ reader.onload = function (e) {
         const myAlert = document.getElementById("myAlert");
 
         if (pincode_details === undefined) {
+            // location.reload();
             myAlert.style.display = "block";
-            
+
             // Hide the alert after 5 seconds
-            setTimeout(function() {
+            setTimeout(function () {
                 myAlert.style.display = "none";
-            }, 5000);
+            }, 10000);
         } else {
             myAlert.style.display = "none";
             calculatePrice();
         }
     });
 
+    // Function to calculate the price
     function calculatePrice() {
         const weightInput = document.getElementById("weight");
         const weight = weightInput.value;
@@ -82,17 +104,20 @@ reader.onload = function (e) {
 
         const pincode_details = pincode_details_json[pincode];
 
-        const Facility_State = pincode_details["Facility State"]
+        const Facility_State = pincode_details["Facility State"];
 
         if (state_of_zone_North_East.includes(Facility_State)) {
             var price_state = per_kg_price_of_zone_North_East;
-            var state_zone = "North_East"
+            var state_zone = "North_East";
         } else if (state_of_zone_South.includes(Facility_State)) {
             var price_state = per_kg_price_of_zone_west;
-            var state_zone = "South"
-        } else {
+            var state_zone = "South";
+        } else if (state_of_zone_west.includes(Facility_State)){
             var price_state = per_kg_price_of_zone_South;
-            var state_zone = "West"
+            var state_zone = "West";
+        } else if (state_of_zone_Rest_of_India.includes(Facility_State)){
+            var price_state = per_kg_price_of_zone_Rest_of_India;
+            var state_zone = "Rest_of_India";
         }
 
         const lengthInput = document.getElementById("length");
@@ -112,7 +137,6 @@ reader.onload = function (e) {
         const dimension_weight_input_element = document.getElementById("dimensionWeightInput");
         dimension_weight_input_element.value = `${weight_dimension}`;
 
-
         if (weight > weight_dimension) {
             var new_weight = weight;
             weightInput.classList.add("bg-warning");
@@ -123,12 +147,35 @@ reader.onload = function (e) {
             weightInput.classList.remove("bg-warning");
         }
 
-        const totalamount = price_state * new_weight;
+        if (new_weight < 20){
+            if (state_zone == "North_East") {
+                var totalamount = Minimum_price_of_zone_North_East;
+            }
+            else if (state_zone == "west") {
+                var totalamount = Minimum_price_of_zone_west;
+            }
+            else if (state_zone == "South") {
+                var totalamount = Minimum_price_of_zone_South;
+            }
+            else if (state_zone == "Rest_of_India") {
+                var totalamount = Minimum_price_of_zone_Rest_of_India;
+            }
 
-        const is_ODA = pincode_details["ODA\r"]
+            price_state = "*Below 20Kg Minimum Charge applied Rs " + totalamount + "/-";
+            const resultElement = document.getElementById("price");
+            resultElement.textContent = `${price_state}`;
+        }
+        else {
+            var totalamount = price_state * new_weight;
+            const resultElement = document.getElementById("price");
+            resultElement.textContent = `Rs ${price_state}/- Per Kg`;    
+        }
+
+
+        const is_ODA = pincode_details["ODA\r"];
         const is_ODAElement = document.getElementById("is_ODA");
         if (is_ODA === "TRUE") {
-            var ODA_charge = 550;
+            var ODA_charge = ODA_ORDER_CHARGE;
             var Final_amount = totalamount + ODA_charge;
             is_ODAElement.classList.remove("d-none");
         } else {
@@ -137,11 +184,8 @@ reader.onload = function (e) {
             is_ODAElement.classList.add("d-none");
         }
 
-        const resultElement = document.getElementById("price");
-        resultElement.textContent = `Rs ${price_state}/-`;
-
         const ODA_chargeElement = document.getElementById("ODA_charge");
-        ODA_chargeElement.textContent = `Rs ${ODA_charge}/-`
+        ODA_chargeElement.textContent = `Rs ${ODA_charge}/-`;
 
         const totalElement = document.getElementById("finalAmount");
         totalElement.textContent = `Rs ${Final_amount}/-`;
@@ -151,6 +195,7 @@ reader.onload = function (e) {
     }
 };
 
+// Fetch the CSV file
 fetch(csvFilePath)
     .then(response => response.text())
     .then(csvData => {
