731ff799438c2832c515961fc003365194780da2
Harsh Patel <hrspatel1111@gmail.com>
2023-07-11 14:57:24 +0530

    addded mobile responsive css

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // Constants
 const ODA_ORDER_CHARGE = 600;
 
-const per_kg_price_of_zone_North_East = 35;
-const per_kg_price_of_zone_west = 15;
-const per_kg_price_of_zone_South = 28;
-const per_kg_price_of_zone_Rest_of_India = 25;
+const Per_KG_Charge_of_Zone_North_East = 35;
+const Per_KG_Charge_of_Zone_West = 15;
+const Per_KG_Charge_of_Zone_South = 28;
+const Per_KG_Charge_of_Zone_Rest_of_India = 25;
 
-const Minimum_price_of_zone_North_East = 550;
-const Minimum_price_of_zone_west = 350;
-const Minimum_price_of_zone_South = 500;
-const Minimum_price_of_zone_Rest_of_India = 450;
+const Minimum_Charge_of_Zone_North_East = 550;
+const Minimum_Charge_of_Zone_West = 350;
+const Minimum_Charge_of_Zone_South = 500;
+const Minimum_Charge_of_Zone_Rest_of_India = 450;
 
-const state_of_zone_North_East = ["Assam", "Mizoram", "Manipur", "Nagaland", "Jammu & Kashmir", "Arunachal Pradesh", "Meghalaya", "Tripura"];
-const state_of_zone_west = ["Gujarat", "Maharashtra", "Goa", "Madhya Pradesh", "Chhattisgarh", "Dadra and Nagar Haveli", "Rajasthan"];
-const state_of_zone_South = ["Andhra Pradesh", "Karnataka", "Telangana", "Pondicherry", "Tamil Nadu", "Kerala"];
-const state_of_zone_Rest_of_India = ["Delhi", "Uttar Pradesh", "Punjab", "Orissa", "Himachal Pradesh", "Uttarakhand", "Haryana", "Bihar", "Jharkhand", "West Bengal"];
+const State_of_Zone_North_East = ["Assam", "Mizoram", "Manipur", "Nagaland", "Jammu & Kashmir", "Arunachal Pradesh", "Meghalaya", "Tripura"];
+const State_of_Zone_West = ["Gujarat", "Maharashtra", "Goa", "Madhya Pradesh", "Chhattisgarh", "Dadra and Nagar Haveli", "Rajasthan"];
+const State_of_Zone_South = ["Andhra Pradesh", "Karnataka", "Telangana", "Pondicherry", "Tamil Nadu", "Kerala"];
+const State_of_Zone_Rest_of_India = ["Delhi", "Uttar Pradesh", "Punjab", "Orissa", "Himachal Pradesh", "Uttarakhand", "Haryana", "Bihar", "Jharkhand", "West Bengal"];
 
 const csvFilePath = 'data.csv';
 
@@ -57,6 +57,40 @@ reader.onload = function (e) {
     const pincode_details_json = csv_to_json(csvData);
     console.log(pincode_details_json)
 
+    // Fumction to find zone data by pincode
+    function find_zone_by_pincode(weight, pincode_data) {
+        console.log(pincode_data)
+        console.log(weight)
+
+        const Facility_State = pincode_data["Facility State"];
+        console.log(Facility_State)
+
+
+        if (State_of_Zone_North_East.includes(Facility_State)) {
+            var state_zone = "North_East";
+            var minimum_weight = Minimum_Charge_of_Zone_North_East/Per_KG_Charge_of_Zone_North_East;
+            var minimum_charge = Minimum_Charge_of_Zone_North_East;
+            var charge = Per_KG_Charge_of_Zone_North_East;
+        } else if (State_of_Zone_West.includes(Facility_State)){
+            var state_zone = "West";
+            var minimum_weight = Minimum_Charge_of_Zone_West/Per_KG_Charge_of_Zone_West;
+            var minimum_charge = Minimum_Charge_of_Zone_West;
+            var charge = Per_KG_Charge_of_Zone_West;
+        } else if (State_of_Zone_South.includes(Facility_State)) {
+            var state_zone = "South";
+            var minimum_weight = Minimum_Charge_of_Zone_South/Per_KG_Charge_of_Zone_South;
+            var minimum_charge = Minimum_Charge_of_Zone_South;
+            var charge = Per_KG_Charge_of_Zone_South;
+        } else if (State_of_Zone_Rest_of_India.includes(Facility_State)){
+            var state_zone = "Rest_of_India";
+            var minimum_weight = Minimum_Charge_of_Zone_Rest_of_India/Per_KG_Charge_of_Zone_Rest_of_India;
+            var minimum_charge = Minimum_Charge_of_Zone_Rest_of_India;
+            var charge = Per_KG_Charge_of_Zone_Rest_of_India;
+        }
+
+        return [state_zone, charge, minimum_weight, minimum_charge]
+    }
+
     // Event listener for pincode input
     document.getElementById("pincode").addEventListener("input", function () {
         const pincodeInput = document.getElementById("pincode");
@@ -90,12 +124,12 @@ reader.onload = function (e) {
             }, 10000);
         } else {
             myAlert.style.display = "none";
-            calculatePrice();
+            calculateCharge();
         }
     });
 
-    // Function to calculate the price
-    function calculatePrice() {
+    // Function to calculate the charge
+    function calculateCharge() {
         const weightInput = document.getElementById("weight");
         const weight = weightInput.value;
 
@@ -103,23 +137,7 @@ reader.onload = function (e) {
         const pincode = pincodeInput.value;
 
         const pincode_details = pincode_details_json[pincode];
-
-        const Facility_State = pincode_details["Facility State"];
-
-        if (state_of_zone_North_East.includes(Facility_State)) {
-            var price_state = per_kg_price_of_zone_North_East;
-            var state_zone = "North_East";
-        } else if (state_of_zone_South.includes(Facility_State)) {
-            var price_state = per_kg_price_of_zone_west;
-            var state_zone = "South";
-        } else if (state_of_zone_west.includes(Facility_State)){
-            var price_state = per_kg_price_of_zone_South;
-            var state_zone = "West";
-        } else if (state_of_zone_Rest_of_India.includes(Facility_State)){
-            var price_state = per_kg_price_of_zone_Rest_of_India;
-            var state_zone = "Rest_of_India";
-        }
-
+        
         const lengthInput = document.getElementById("length");
         const length = lengthInput.value;
 
@@ -147,40 +165,33 @@ reader.onload = function (e) {
             weightInput.classList.remove("bg-warning");
         }
 
-        if (new_weight < 20){
-            if (state_zone == "North_East") {
-                var totalamount = Minimum_price_of_zone_North_East;
-            }
-            else if (state_zone == "west") {
-                var totalamount = Minimum_price_of_zone_west;
-            }
-            else if (state_zone == "South") {
-                var totalamount = Minimum_price_of_zone_South;
-            }
-            else if (state_zone == "Rest_of_India") {
-                var totalamount = Minimum_price_of_zone_Rest_of_India;
-            }
+        const [state_zone_name, zone_charge, minimum_zone_weight, minimum_zone_charge] = find_zone_by_pincode(new_weight, pincode_details)
 
-            const resultElement = document.getElementById("price");
-            resultElement.textContent = `Rs ${totalamount}/-`;
+        const resultElement = document.getElementById("charge");
+        const Minimum_WeightElement = document.getElementById("minimum_weight");
+        if (new_weight < Math.ceil(minimum_zone_weight)){
+            Minimum_WeightElement.classList.remove("hide_element");
+            Minimum_WeightElement.textContent = `*Below ${Math.ceil(minimum_zone_weight)}Kg minimum charges will apply.`
+            var total_amount = minimum_zone_charge;
+            resultElement.textContent = `Rs ${total_amount}/-`;
+        } 
+        else{
+            Minimum_WeightElement.classList.add("hide_element");
+            var total_amount = new_weight * zone_charge
+            resultElement.textContent = `Rs ${total_amount}/- (${new_weight} Kg * ${zone_charge} Rs)`;
         }
-        else {
-            var totalamount = price_state * new_weight;
-            const resultElement = document.getElementById("price");
-            resultElement.textContent = `Rs ${totalamount}/- (${new_weight} Kg * ${price_state} Rs)`;
-        }
-
+        
 
         const is_ODA = pincode_details["ODA\r"];
         const is_ODAElement = document.getElementById("is_ODA");
         if (is_ODA === "TRUE") {
             var ODA_charge = ODA_ORDER_CHARGE;
-            var Final_amount = totalamount + ODA_charge;
-            is_ODAElement.classList.remove("d-none");
+            var Final_amount = total_amount + ODA_charge;
+            is_ODAElement.classList.remove("hide_element");
         } else {
             var ODA_charge = 0;
-            var Final_amount = totalamount;
-            is_ODAElement.classList.add("d-none");
+            var Final_amount = total_amount;
+            is_ODAElement.classList.add("hide_element");
         }
 
         const ODA_chargeElement = document.getElementById("ODA_charge");
@@ -194,9 +205,9 @@ reader.onload = function (e) {
 
         // Store data in localStorage
         localStorage.setItem('weight', weight);
-        localStorage.setItem('price', price_state);
+        localStorage.setItem('charge', zone_charge);
         localStorage.setItem('ODA', ODA_charge);
-        localStorage.setItem('totalAmount', Final_amount);
+        localStorage.setItem('finalAmount', Final_amount);
         localStorage.setItem('pincode', pincode);
     }
 
@@ -204,16 +215,16 @@ reader.onload = function (e) {
     $('#orderModalDetails').on('show.bs.modal', function (e) {
         // Retrieve data from localStorage
         const weight = localStorage.getItem('weight');
-        const price = localStorage.getItem('price');
+        const charge = localStorage.getItem('charge');
         const ODA = localStorage.getItem('ODA');
-        const totalAmount = localStorage.getItem('totalAmount');
+        const finalAmount = localStorage.getItem('finalAmount');
         const pincode = localStorage.getItem('pincode');
 
         // Update modal fields with retrieved data
         document.getElementById('modal_weight').value = weight;
-        document.getElementById('modal_price').value = price;
+        document.getElementById('modal_charge').value = charge;
         document.getElementById('modal_ODA').value = ODA;
-        document.getElementById('modal_total_amount').value = totalAmount;
+        document.getElementById('modal_total_amount').value = finalAmount;
         document.getElementById('modal_pincode').value = pincode;
     });
 
@@ -221,7 +232,7 @@ reader.onload = function (e) {
     $('#orderModalDetails').on('reload', function (e) {
         // Clear data from localStorage
         localStorage.removeItem('weight');
-        localStorage.removeItem('price');
+        localStorage.removeItem('charge');
         localStorage.removeItem('ODA');
         localStorage.removeItem('totalAmount');
         localStorage.removeItem('pincode');
@@ -232,7 +243,7 @@ reader.onload = function (e) {
         // Get the values from the input fields
         const Weight = document.getElementById("modal_weight").value;
         const Pincode = document.getElementById("pincode").value;
-        const Price = document.getElementById("modal_price").value;
+        const Charge = document.getElementById("modal_charge").value;
         const ODA = document.getElementById("modal_ODA").value;
         const TotalAmount = document.getElementById("modal_total_amount").value;
         const senderFname = document.getElementById("sender_fname").value;
@@ -245,7 +256,7 @@ reader.onload = function (e) {
         const receiverAddress = document.getElementById("receiver_address").value;
     
         // Construct the WhatsApp URL with the parameters
-        const whatsappURL = `https://wa.me/+919586010006?text=Order%20Details%0A%0APincode%3A%20${Pincode}%0AWeight%3A%20${Weight}%0APrice%3A%20${Price}%0AODA%20Charge%3A%20${ODA}%0ATotal%20Amount%3A%20${TotalAmount}%0A%0ASender%27s%20Details%3A%0AName%3A%20${senderFname}%20${senderLname}%0APhone%20Number%3A%20${senderPnumber}%0AAddress%3A%20${senderAddress}%0A%0AReceiver%27s%20Details%3A%0AName%3A%20${receiverFname}%20${receiverLname}%0APhone%20Number%3A%20${receiverPnumber}%0AAddress%3A%20${receiverAddress}`;
+        const whatsappURL = `https://wa.me/+919586010006?text=Order%20Details%0A%0APincode%3A%20${Pincode}%0AWeight%3A%20${Weight}%0Acharge%3A%20${Charge}%0AODA%20Charge%3A%20${ODA}%0ATotal%20Amount%3A%20${TotalAmount}%0A%0ASender%27s%20Details%3A%0AName%3A%20${senderFname}%20${senderLname}%0APhone%20Number%3A%20${senderPnumber}%0AAddress%3A%20${senderAddress}%0A%0AReceiver%27s%20Details%3A%0AName%3A%20${receiverFname}%20${receiverLname}%0APhone%20Number%3A%20${receiverPnumber}%0AAddress%3A%20${receiverAddress}`;
     
         // Open the WhatsApp URL in a new window or tab
         window.open(whatsappURL);
